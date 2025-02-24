929459679e5b9defdf4d42c8b20fba3b229915a9
Владимир Миргородский <mirgorodscky.volodymyr@yandex.ru>
2025-02-24 19:42:49 +0300

    Initial commit

---
+++ src/main/java/com/example/anton_task1/AntonTask1Application.java
@@ -0,0 +1,13 @@
+package com.example.anton_task1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntonTask1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntonTask1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/anton_task1/AntonTask1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.anton_task1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntonTask1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
